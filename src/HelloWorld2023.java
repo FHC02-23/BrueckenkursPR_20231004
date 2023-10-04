2ea1590d60db2f6464dc2dcbb4cbc05adf02087f
Stephan Kochauf, Campus02 <stephan.kochauf@lv.campus02.at>
2023-10-04 19:12:23 +0200

    Hello World

---
+++ src/HelloWorld2023.java
@@ -0,0 +1,15 @@
+public class HelloWorld2023 {
+    // main
+    public static void main(String[] args) { // Hauptprogramm
+
+        /*
+        mehrzeiliger Kommentar
+        asdf
+         */
+
+        // sout
+        System.out.print("Hello ");
+        System.out.println("World");
+
+    }
+}
